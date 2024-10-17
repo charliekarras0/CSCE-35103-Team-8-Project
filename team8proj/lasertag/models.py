1a6c3168ec2937d3634286f4de2935a81f52f655
br1husong <112578746+br1husong@users.noreply.github.com>
2024-10-17 14:19:41 -0500

    Delete team8proj directory, cleaning for sprint 3

--- team8proj/lasertag/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Player(models.Model):
-    id = models.IntegerField(primary_key=True)
-    codename = models.CharField(max_length=255)
-
-    class Meta:
-        db_table = 'players'
